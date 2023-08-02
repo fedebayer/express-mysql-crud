6fcf11d209b75dd362a8cc3433c3817e378f7302
FedeBayer <fbayerque@alumnos.exa.unicen.edu.ar>
2023-08-02 20:41:46 -0300

    Refactor structure

--- src/index.js
+++ src/index.js
@@ -1,21 +1,5 @@
-import express from "express";
-import { PORT } from "../config.js";
-
-import {
-  default as employeesRoutes,
-  default as indexRoutes,
-} from "./routes/employees.routes.js";
-
-const app = express();
-
-app.use(express.json());
-
-app.use(indexRoutes);
-app.use("/api", employeesRoutes);
-
-app.use((req, res, next) => {
-  res.status(404).json({ message: "Endpoint not found" });
-});
+import app from "./app.js";
+import { PORT } from "./config.js";
 
 app.listen(PORT);
 console.log("Server running on port", PORT);
